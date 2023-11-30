6e74b50ac656ef4e9f773dd54be496b2d23293a2
xiaoyuanda <xiaoyuanda@aliyun.com>
2023-11-30 14:04:22 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const h = ref(null)
+
+onMounted(()=>{
+  console.log(h.value)
+})
+</script>
+
+<template>
+  <h1 ref="h">hello</h1>
+</template>
+
+<style scoped>
+</style>
